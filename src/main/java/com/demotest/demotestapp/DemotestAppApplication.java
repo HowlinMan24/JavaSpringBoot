e7b18550db4ef0e5679ab16bd479fa93d108bcc7
Hristijan Mijalkov <hristijan.mijalkov@vista-point.io>
2024-09-01 16:10:01 +0200

    first commit

---
+++ src/main/java/com/demotest/demotestapp/DemotestAppApplication.java
@@ -0,0 +1,13 @@
+package com.demotest.demotestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemotestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemotestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demotest/demotestapp/DemotestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demotest.demotestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemotestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
